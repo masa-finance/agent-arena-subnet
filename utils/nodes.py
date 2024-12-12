b2de03e6a53a9783b1a9e20549a9a8e6af90d9d8
Grant Foster <grantdfoster@gmail.com>
2024-12-11 16:10:36 -0800

    feat: adds miner endpoint to deregister your agent (for testing for now)

--- utils/nodes.py
+++ utils/nodes.py
@@ -5,13 +5,11 @@ from typing import Dict
 
 
 def format_nodes_to_dict(raw_nodes: Dict[str, Node]) -> list[Node]:
-    print("Format nodes to dict *****************************")
     nodes = [Node(**node.__dict__) for node in raw_nodes.values()]
     return nodes
 
 
 def filter_nodes_with_ip_and_port(nodes: list[Node]) -> list[Node]:
     """Filter nodes that have both IP and port defined."""
-    filtered_nodes = [node for node in nodes if node.ip !=
-                      "0.0.0.0" and node.port != 0]
+    filtered_nodes = [node for node in nodes if node.ip != "0.0.0.0" and node.port != 0]
     return filtered_nodes
