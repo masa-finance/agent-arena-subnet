aa4b58c74e7b9afa0216b2c64f2aea4378074afd
Grant Foster <grantdfoster@gmail.com>
2024-12-18 11:21:06 -0800

    fix: post req

--- neurons/validator.py
+++ neurons/validator.py
@@ -320,6 +320,9 @@ class AgentValidator:
             symmetric_key_uuid=registered_node.symmetric_key_uuid,
             endpoint="/registration_callback",
             validator_ss58_address=self.keypair.ss58_address,
+            miner_ss58_address=node.hotkey,
+            keypair=self.keypair,
+            fernet=registered_node.fernet,
             payload=json.dumps(
                 {"Successfully Registered Agent: ": agent}, default=lambda o: o.__dict__
             ),
